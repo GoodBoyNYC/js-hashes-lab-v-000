d3bdcafc1599bbb12887425208f64ee1d174331e
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-26 12:48:45 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -3,5 +3,21 @@
 function addIngredient(recipeObj, ingredientKey, amountsValue) {
   recipeObj[ingredientKey]=amountsValue;
   return recipeObj;
+};
 
-}
+function removeIngredient(recipeObj, ingredientKey) {
+  delete recipeObj[ingredientKey];
+  return recipeObj;
+};
+
+function updateIngredient(recipeObj, ingredientKey, amountsValue) {
+  recipeObj[ingredientKey]=amountsValue;
+  return recipeObj;
+};
+
+function readRecipe(recipeObj){
+  for(var ingredientKey in recipeObj){
+    //console.log(`this recipe calls for ${recipeObj[ingredientKey]} of ${ingredientKey}`);
+    console.log("this recipe calls for " + recipeObj[ingredientKey] + " of " + ingredientKey);
+  }
+};
